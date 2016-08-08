50fb88d25aef62199bbc162192998b361a33a8e9
Nick Koslowski <nko@hup.de>
2016-08-08 14:00:07 +0200

    add header

--- Example/FontAwesomeSwift/FontAwesomeSwift.h
+++ Example/FontAwesomeSwift/FontAwesomeSwift.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double FontAwesomeSwiftVersionNumber;
 FOUNDATION_EXPORT const unsigned char FontAwesomeSwiftVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FontAwesomeSwift/PublicHeader.h>
-
+#import "FontAwesomeSwift-Swift.h"
 
