81eddd70842033d9af78465981b3af5a277d334b
Nick Koslowski <nko@hup.de>
2016-08-08 13:51:55 +0200

    Add carthage

---
+++ Example/FontAwesomeSwift/FontAwesomeSwift.h
@@ -0,0 +1,19 @@
+//
+//  FontAwesomeSwift.h
+//  FontAwesomeSwift
+//
+//  Created by Nick Koslowski on 08/08/16.
+//  Copyright © 2016 Patrik Vaberer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontAwesomeSwift.
+FOUNDATION_EXPORT double FontAwesomeSwiftVersionNumber;
+
+//! Project version string for FontAwesomeSwift.
+FOUNDATION_EXPORT const unsigned char FontAwesomeSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontAwesomeSwift/PublicHeader.h>
+
+
